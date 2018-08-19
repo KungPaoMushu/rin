73614c523f39a12add023be6b1895ede6206430f
KungPaoMushu <KPMCameronC@gmail.com>
2018-08-19 12:50:16 -0700

    photo added, MemoriesScreen nonworking button

--- screens/MemoriesScreen.js
+++ screens/MemoriesScreen.js
@@ -1,22 +1,51 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Button, Text, Image } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
+const counter = 0; 
+const cam = require('../assets/images/cam.png'); 
+const other = require('../assets/images/icon.png')
+
+
+const text1 = 'text1'; 
+const text2 = 'text2'
+var pictureDisplay = false; 
 export default class MemoriesScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Memories',
   };
 
+  
+
+onPressButton() {
+	var first = pictureDisplay; 
+	var second = !first; 
+	pictureDisplay: second; 
+}
+
   render() {
+  	
+  	let imageSource = pictureDisplay ? cam : other; 
+  	let displayedText = pictureDisplay ? text1 : text2; 
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+       
+        <Button
+            onPress={this.onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+         
+
+         
+         <Image source= {imageSource} />
+         <Text> {displayedText} </Text>
+          
       </ScrollView>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
