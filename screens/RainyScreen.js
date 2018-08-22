9242648410bd3530079f32bb8a3cd485c97caffa
KungPaoMushu <KPMCameronC@gmail.com>
2018-08-21 23:42:54 -0700

    RainyScreen: Button, Text, and Container styles added

--- screens/RainyScreen.js
+++ screens/RainyScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Button, Alert, Image } from 'react-native';
+import { ScrollView, StyleSheet, Button, Alert, Image, View, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
@@ -13,13 +13,22 @@ export default class LinksScreen extends React.Component {
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
       <Image source= {require('../assets/images/sadUnicorn.jpg')} />
+      <View style = {styles.cheerUpButtonContainer}>
         <Button style = {styles.cheerUpButton}
   onPress={() => {
         Alert.alert('You tapped the button!');
       }}
       title="Cheer Up"
-      color="#841584"
+      color="#fff"
     />
+    </View>
+
+    <View style = {styles.cheerUpTextContainer}>
+      
+      <Text style = {styles.cheerUpText}>
+        Text goes here
+      </Text> 
+    </View>
       </ScrollView>
     );
   }
@@ -29,13 +38,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#bc8abd',
   },
   center: {
     alignItems: 'center',
     
   },
-  cheerUpButton: {
-    paddingTop: 40,
+  cheerUpButtonContainer: {
+    marginTop: 25,
+    backgroundColor: '#eeccff',
+    
+  },
+
+  cheerUpTextContainer:
+  {
+    backgroundColor: '#eeccff',
+    margin: 15, 
+    paddingTop: 15, 
+    paddingBottom: 15, 
+    paddingLeft: 100,
+    paddingRight: 100
+  }, 
+
+  cheerUpText:
+  {
+    color: '#fff'
   }
 });
